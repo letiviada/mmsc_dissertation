3e8dce3eead77898672e6d738b9fcee6fc361d45
Leticia Viada <lviadacampos@gmail.com>
2024-05-29 18:14:38 +0100

    Add figures

--- trouton_model/main.py
+++ trouton_model/main.py
@@ -15,18 +15,19 @@ x_sol,z_sol = solver(nx,t_eval)
 #plot_results(x_eval,t_eval,z_sol, option = 'space', variable='axial velocity')
 
     # Create a new figure with subplots
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10,8),dpi = 300)
     
     # Plot thickness against time
 plot_results(x_eval, t_eval, x_sol, option='time', variable='sheet thickness', ax=ax1, fig=fig)
     
     # Plot thickness against space
 plot_results(x_eval, t_eval, x_sol, option='space', variable='sheet thickness', ax=ax2, fig=fig)
+fig.savefig('dae_examples/trouton_model/figures/thickness.png')
     
 plt.show()
 
     # Create a new figure with subplots
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8),dpi =300)
     
     # Plot thickness against time
 plot_results(x_eval, t_eval, x_sol, option='time', variable='axial velocity', ax=ax1, fig=fig)
@@ -34,4 +35,5 @@ plot_results(x_eval, t_eval, x_sol, option='time', variable='axial velocity', ax
     # Plot thickness against space
 plot_results(x_eval, t_eval, x_sol, option='space', variable='axial velocity', ax=ax2, fig=fig)
     
+fig.savefig('dae_examples/trouton_model/figures/axial_velocity.png')
 plt.show()
