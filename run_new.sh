aad16e30283b36d6f6e947aa3b06e6cd34dda98d
Leticia Viada <lviadacampos@gmail.com>
2024-06-23 23:53:17 +0100

    Initial commit

--- run_new.sh
+++ run_new.sh
@@ -3,16 +3,7 @@
 # Define values for alpha and beta
 betas=$(seq 0 0.01 0.1)
 alphas=$(seq 0 0.1 1)
-
-#computation_start_time=$(date +%s)
 # Run the Python script with the list of alphas and betas
-#python3 multiscale/micro_main1.py --alphas $alphas --betas $betas
-#computation_end_time=$(date +%s)
-# Calculate and display the computation time
-#computation_time=$((computation_end_time - computation_start_time))
-#echo "Computation time: $computation_time seconds"
-# Combine individual result files into one JSON file
-#python3 multiscale/combine_results.py
 
 #echo "Done!"
 #
@@ -21,18 +12,18 @@ alphas=$(seq 0 0.1 1)
 script_start_time=$(date +%s)
 
 # Start timing the micro computation
-#micro_computation_start_time=$(date +%s)
-#python3 multiscale/micro_main.py --alphas $alphas --betas $betas
-#micro_computation_end_time=$(date +%s)
-#micro_computation_time=$((micro_computation_end_time - micro_computation_start_time))
-#echo "Micro computation time: $micro_computation_time seconds"
+micro_computation_start_time=$(date +%s)
+python3 multiscale/micro_main.py --alphas $alphas --betas $betas
+micro_computation_end_time=$(date +%s)
+micro_computation_time=$((micro_computation_end_time - micro_computation_start_time))
+echo "Micro computation time: $micro_computation_time seconds"
 
 # Start timing the macro computation
-#macro_computation_start_time=$(date +%s)
-#python3 multiscale/macro_main.py --alphas $alphas --betas $betas
-#macro_computation_end_time=$(date +%s)
-#macro_computation_time=$((macro_computation_end_time - macro_computation_start_time))
-#echo "Macro computation time: $macro_computation_time seconds"
+macro_computation_start_time=$(date +%s)
+python3 multiscale/macro_main.py --alphas $alphas --betas $betas
+macro_computation_end_time=$(date +%s)
+macro_computation_time=$((macro_computation_end_time - macro_computation_start_time))
+echo "Macro computation time: $macro_computation_time seconds"
 
 # Combine individual micro result files into one JSON file and delete them
 micro_combine_start_time=$(date +%s)
@@ -48,4 +39,5 @@ total_time=$((script_end_time - script_start_time))
 echo "Total execution time: $total_time seconds"
 
 echo "Done!"
+
 
