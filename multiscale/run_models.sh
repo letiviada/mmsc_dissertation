840002fa624396a8fe04b6a239d507408a5fe1d2
Leticia Viada <lviadacampos@gmail.com>
2024-07-15 16:17:52 +0100

    Add

--- multiscale/run_models.sh
+++ multiscale/run_models.sh
@@ -2,9 +2,9 @@
 # To run, put in the terminal: bash micro_params.sh
 # Define values for alpha and beta
 #
-alphas=$(seq 0.6 0.04 0.8)
-betas=$(seq 0.027 0.001 0.028)
-#betas=(0.027)
+alphas=$(seq 0.3 0.02 1.0)
+#betas=$(seq 0.03 0.002 0.4)
+betas=(0.05)
 phis=(1.0)
 num_runs=1
 
@@ -15,7 +15,7 @@ script_start_time=$(date +%s)
 # Start timing the micro computation
 micro_computation_start_time=$(date +%s)
 echo "Running micro simulation..."
-python3 multiscale/microscale/micro_main.py --alphas $alphas --betas $betas --num_runs $num_runs
+#python3 multiscale/microscale/micro_main.py --alphas $alphas --betas $betas --num_runs $num_runs
 micro_computation_end_time=$(date +%s)
 micro_computation_time=$((micro_computation_end_time - micro_computation_start_time))
 minutes=$((micro_computation_time / 60))
@@ -24,7 +24,7 @@ echo "Micro computation time: $minutes minutes and $seconds seconds"
 
 # Combine individual micro result files into one JSON file and delete them
 micro_combine_start_time=$(date +%s)
-python3 multiscale/combine_results.py micro --num_runs $num_runs
+#python3 multiscale/combine_results.py micro --num_runs $num_runs
 micro_combine_end_time=$(date +%s)
 micro_combine_time=$((micro_combine_end_time - micro_combine_start_time))
 minutes=$((micro_combine_time / 60))
@@ -34,7 +34,7 @@ echo "Combining micro_results time: $minutes minutes and $seconds seconds"
 # Start timing the macro computation
 macro_computation_start_time=$(date +%s)
 echo "Running macro simulation..."
-python3 multiscale/macroscale/macro_main.py --alphas $alphas --betas $betas --phis $phis --num_runs $num_runs
+#python3 multiscale/macroscale/macro_main.py --alphas $alphas --betas $betas --phis $phis --num_runs $num_runs
 macro_computation_end_time=$(date +%s)
 macro_computation_time=$((macro_computation_end_time - macro_computation_start_time))
 minutes=$((macro_computation_time / 60))
@@ -43,7 +43,7 @@ echo "Macro computation time: $minutes minutes and $seconds seconds"
 
 # Combine individual micro result files into one JSON file and delete them
 combine_start_time=$(date +%s)
-python3 multiscale/combine_results.py macro_phi --num_runs $num_runs
+#python3 multiscale/combine_results.py macro_phi --num_runs $num_runs
 combine_end_time=$(date +%s)
 combine_time=$((combine_end_time - combine_start_time))
 minutes=$((combine_time / 60))
