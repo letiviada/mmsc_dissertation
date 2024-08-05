e75b5d0148a4e01a8f503203f29bc3a48be925eb
Leticia Viada <lviadacampos@gmail.com>
2024-08-05 09:10:26 +0100

    Add LHS

--- multiscale/run_models.sh
+++ multiscale/run_models.sh
@@ -4,7 +4,7 @@
 # Define values for alpha and beta
 #
 
-alphas=$(seq 0.0 0.1 1.0)
+alphas=$(seq 0.1 0.1 1.0)
 betas=$(seq 0.01 0.01 0.1)
 #betas=(0.02)
 #alphas=(0.6)
@@ -37,7 +37,7 @@ echo "Combining micro_results time: $minutes minutes and $seconds seconds"
 # Start timing the macro computation
 macro_computation_start_time=$(date +%s)
 echo "Running macro simulation..."
-#python3 multiscale/macroscale/macro_main.py --alphas $alphas --betas $betas --phis $phis --num_runs $num_runs
+python3 multiscale/macroscale/macro_main.py --alphas $alphas --betas $betas --phis $phis --num_runs $num_runs
 macro_computation_end_time=$(date +%s)
 macro_computation_time=$((macro_computation_end_time - macro_computation_start_time))
 minutes=$((macro_computation_time / 60))
@@ -46,7 +46,7 @@ echo "Macro computation time: $minutes minutes and $seconds seconds"
 
 # Combine individual micro result files into one JSON file and delete them
 combine_start_time=$(date +%s)
-#python3 multiscale/combine_results.py macro_phi --num_runs $num_runs
+python3 multiscale/combine_results.py macro_phi --num_runs $num_runs
 combine_end_time=$(date +%s)
 combine_time=$((combine_end_time - combine_start_time))
 minutes=$((combine_time / 60))
@@ -56,7 +56,7 @@ echo "Combining results time: $minutes minutes and $seconds seconds"
 # Performance indicators computation
 perf_indicators_start=$(date +%s)
 echo "Running performance indicators computation..."
-#python3 multiscale/performance_indicators/perfo_indicators.py --alphas $alphas --betas $betas --phis $phis --num_runs $num_runs
+python3 multiscale/performance_indicators/perfo_indicators.py --alphas $alphas --betas $betas --phis $phis --num_runs $num_runs
 perf_indicators_end=$(date +%s)
 perf_indicators_time=$((perf_indicators_end - perf_indicators_start))
 minutes=$((perf_indicators_end_time / 60))
@@ -65,7 +65,7 @@ echo "Performance Indicators computation time: $minutes minutes and $seconds sec
 
 # Combine individual micro result files into one JSON file and delete them
 combine_start_time=$(date +%s)
-#python3 multiscale/combine_results.py performance_indicators --num_runs $num_runs
+python3 multiscale/combine_results.py performance_indicators --num_runs $num_runs
 combine_end_time=$(date +%s)
 combine_time=$((combine_end_time - combine_start_time))
 minutes=$((combine_time / 60))
